80f91fcb1bc6c0d7884cc1bf40f0fd5c3e27212d
Teem <dtrkabiling@gmail.com>
2024-02-14 16:36:40 -0400

    update proj

--- index.js
+++ index.js
@@ -2783,13 +2783,132 @@ for(let i = 0; i < countOneToTen.length; i++) {
     countByTwo.push(count * 2);
 }
 
-console.log(countByTwo);
+// console.log(countByTwo);
 
 //--------------------------------
 
 for(let i = 5; i >= 0; i--) {
-    console.log(i);
+    // console.log(i);
 }
 
 //--------------------------------
 
+// let i = 0;
+
+// while(i <= 5) {
+//     i++;
+//     console.log(i * 2 - 2);
+// }
+
+//--------------------------------
+
+// let i = 5;
+
+// while(i >= 0) {
+//     i--;
+//     console.log(i + 1);
+// }
+
+//--------------------------------
+
+// const arrayValue = [1, 2, 3];
+// const arrayV1 = [];
+
+// for(let i = 0; i < arrayValue.length; i++) {
+//     const plus = arrayValue[i];
+
+//     arrayV1.push(plus + 1);
+// }
+
+// console.log(arrayV1);
+
+//--------------------------------
+
+// function addOne(array) {
+//     const result = [];
+
+//     for(let i = 0; i < array.length; i++) {
+//         result.push(array[i] + 1);
+//     }
+
+//     return result;
+// }
+
+// console.log(addOne([1, 2, 3]));
+// console.log(addOne([-2, -1, 0, 99]));
+
+//--------------------------------
+
+// function addNum(array, num) {
+//     const result = [];
+
+//     for(let i = 0; i < array.length; i++) {
+//         result.push(array[i] + num);
+//     }
+//     return result;
+// }
+
+// console.log(addNum([1,2,3], 2));
+// console.log(addNum([1,2,3], 3));
+// console.log(addNum([-2,-1,0,99], 2));
+
+//--------------------------------
+
+// function addArrays(array1, array2) {
+//     const result = [];
+
+//     for(let i = 0; i < array1.length; i++) {
+//         result.push(array1[i] + array2[i]);
+//     }
+//     return result;
+// }
+
+// console.log(addArrays([1,1,2], [1,1,3]));
+// console.log(addArrays([1,2,3], [4,5,6]));
+
+//--------------------------------
+
+// function countPositive(nums) {
+//     let result = 0;
+
+//     for(i = 0; i < nums.length; i++) {
+
+//         if(nums[i] >= 1) {
+//             result++;
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(countPositive([1, -3, 5]));
+// console.log(countPositive([-2, 3, -5, 7, 10]));
+
+//--------------------------------
+
+function minMax(nums) {
+
+    const result = {
+        min: nums[0],
+        max: nums[0]
+    };
+
+    for(let i = 0; i < nums.length; i++) {
+        const value = nums[i];
+
+        // If the value is less than the min,
+        // update the min.
+        if (value < result.min) {
+        result.min = value;
+        }
+
+        // If the value is greater than the max,
+        // update the max.
+        if (value > result.max) {
+        result.max = value;
+        }
+    }
+    return result;
+}
+
+console.log(minMax([1, -3, 5]));
+console.log(minMax([-2, 3, -5, 7 ,10]));
