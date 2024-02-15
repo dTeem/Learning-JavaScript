f71569f8738d27ef0c487c2b1ed68815574e8930
Teem <dtrkabiling@gmail.com>
2024-02-15 15:45:04 -0400

    mini proj

--- index.js
+++ index.js
@@ -3125,3 +3125,48 @@ function doubleArray(nums) {
 }
 console.log(doubleArray([2,2,0,7,2]));
 */
+
+
+//----------------------------------------
+// --- PROJECTS FOR ARRAYS AND LOOPS ---
+
+
+let words = 
+[
+    'hello',
+    'world',
+    'not',
+    'search',
+    'good',
+    'search'
+];
+let index = -1;
+
+for(let i = 0; i < words.length; i++) 
+{
+    if(words[i] === 'search')
+    {
+        index = i;
+        break;
+    }
+}
+
+console.log(index);
+
+words = ['not', 'found', 'search'];
+index = -1;
+
+for(let i = 0; i < words.length; i++)
+{
+    if(words[i] === 'search')
+    {
+        index = i;
+        break;
+    }
+}
+
+// console.log(index);
+
+//----------------------------------------
+
+
